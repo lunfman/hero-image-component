8b27fd0a764c537e9dd94c96c837412d3077954a
lunfman <smart275@gmail.com>
2022-06-04 20:22:46 +0300

    feat: create new component - HeroImageHeader - component which allow to create heroimage with tilte and subtitle but can not be customized - where tests for this component? -> no tests because it uses combo of prev components

---
+++ src/components/HeroImage/HeroImageHeader.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import HeroImage from './HeroImage'
+import HeroSubtitle from "./HeroSubtitle";
+import HeroTitle from "./HeroTitle";
+
+interface HeroImageHeaderProps{
+    imgUrl: string,
+    title?: string,
+    subtitle?: string,
+}
+
+// document.body.style.margin = '0'
+
+export default function HeroImageHeader(props: HeroImageHeaderProps): JSX.Element{
+    const {imgUrl, title, subtitle} = props
+
+    return(
+        <HeroImage imgUrl={imgUrl}>
+            <HeroTitle>{title}</HeroTitle>
+
+            <HeroSubtitle>{subtitle}</HeroSubtitle>
+        </HeroImage>
+    )
+}
