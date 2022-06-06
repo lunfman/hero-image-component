dedaddb5eb8a9ee9fc1cec7a67c77e6313edee83
lunfman <smart275@gmail.com>
2022-06-06 18:54:23 +0300

    build: Modal build - add imports to index.ts s - update rollup with new css plugin cause modal uses own css file - add packages to commit cause of new plugin

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 import { HeroImage, HeroSubtitle, HeroTitle, HeroImageHeader } from "./HeroImage"
 export{HeroImage, HeroSubtitle, HeroTitle, HeroImageHeader}
+export {default as Modal} from './Modal'
